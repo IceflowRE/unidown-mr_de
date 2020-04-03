38d4c9eddfd2d28ac23eb4c391a946f99810a821
Iceflower <iceflower@gmx.de>
2020-04-03 21:43:56 +0200

    [general] Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with Path('README.rst').open(mode='r', encoding='UTF-8') as reader:
 
 setup(
     name='unidown-mr_de',
-    version='1.0.0',
+    version='1.0.1',
     description='MR german books plugin for unidown.',
     long_description=long_description,
     author='Iceflower S',
